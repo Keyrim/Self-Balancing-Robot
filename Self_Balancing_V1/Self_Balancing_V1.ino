046744f343db4871390971f4cd82a4a94a21447b
Keyrim <theom251@gmail.com>
2019-10-03 21:41:35 +0200

    v1-0

--- Self_Balancing_V1/Self_Balancing_V1.ino
+++ Self_Balancing_V1/Self_Balancing_V1.ino
@@ -1,20 +1,48 @@
 //Program forthe balancing robot
 //Library for the MPU
 #include <Wire.h>   
+unsigned long loop_timer = 0 ;
 
 //Motor conections
-#define pin_sens_m1 2
-#define pin_sens_m2 3
-#define pin_step_m1 4
+//A4988 steps settings
+
+
+
+//Motor 1 is the right motor and m2 is the left one 
+#define pin_sens_m1 3
+#define pin_sens_m2 4
+#define pin_step_m1 2
 #define pin_step_m2 5
 
 //Read angle 
+const int MPU=0x68;  // I2C address of the MPU-6050
 const int frequence = 250; //Frequence of the loop in hz
 //raw data
 float AcX, AcY, AcZ, GyX=0, GyY=0, GyZ=0;
 float X=0, Y=0;
 
+//Moove side motors
+void left_step(bool sens)
+{
+    digitalWrite(pin_sens_m2, sens);
+    digitalWrite(pin_step_m2, HIGH);
+    delay(LOW);
+    digitalWrite(pin_step_m2, LOW);
+}
+void right_step(bool sens)
+{
+    digitalWrite(pin_sens_m1, sens);
+    digitalWrite(pin_step_m1, HIGH);
+    delay(LOW);
+    digitalWrite(pin_step_m1, LOW);
+}
 
+//Timmer motor speed 
+unsigned long pre_timer_left = 0 ;
+unsigned long pre_timer_right = 0 ;
+//The speed is defined in degrees per seconds 
+int left_speed = 0 ;
+int right_speed = 0 ; 
 
 
 void read_mpu()
@@ -54,6 +82,9 @@ void step(int pin, bool sens)
 
 void setup()
 {
+    Serial.begin(115200);
+    Serial.println("Init 1");
+    
     // Wake up the mpu 
     Wire.begin();
     Wire.beginTransmission(MPU);
@@ -76,6 +107,7 @@ void setup()
     Wire.write(0x1B);
     Wire.write(0X08);
     Wire.endTransmission();
+    Serial.println("End init");
 
 }
 
@@ -88,10 +120,18 @@ void loop()
     AcY = asin(AcY/total_vector)*57.32;
     
     //Complementary filter
-    X += GyX / frequence ; 
-    X = X * 0.996 + AcY * 0.004 ;
     Y += GyY / frequence ;
     Y = Y * 0.996 + AcX * 0.004;
+    Serial.println(Y);
+
+    if(Y >= 10)
+    {
+        if(micros() > pre_timer_left +400)
+        {
+            left_step(true);
+            pre_timer_left = micros();
+        }
+    }
 
     //Frequence regulation here 
     while(micros()<loop_timer + 1000000/frequence);
